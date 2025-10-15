e7b08bc974f16c989ae73f7680d5d34cac703762
backoffice-tributosimple <gonzaloacosta883@gmail.com>
2025-10-15 17:07:23 -0300

    Feature: Inicio del proyecto + Rutas y pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'basic',
+        title: 'Pipes Básicos',
+        loadComponent: () => import('./pages/basic-page/basic-page.component')
+    },
+    {
+        path: 'numbers',
+        title: 'Numbers Pipes',
+        loadComponent: () => import('./pages/numbers-page/numbers-page.component')
+    },
+    {
+        path: 'uncommon',
+        title: 'Pipes no tan comunes',
+        loadComponent: () => import('./pages/uncommont-page/uncommont-page.component')
+    },
+    {
+        path: 'custom',
+        title: 'Pipes Personalizados',
+        loadComponent: () => import('./pages/custom-page/custom-page.component')
+    },
+    {
+        path: '**',
+        redirectTo: 'basic'
+    }
+];
---
+++ src/app/pages/uncommont-page/uncommont-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uncommont-page',
+  imports: [],
+  templateUrl: './uncommont-page.component.html',
+})
+export default class UncommontPageComponent { }
