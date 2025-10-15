4d100c4bd69ac92761dcc22a65d46d1c8fed07c7
backoffice-tributosimple <gonzaloacosta883@gmail.com>
2025-10-15 17:22:25 -0300

    Feature: Navbar + Lectura de rutas dinamicamente.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import BasicPageComponent from './pages/basic-page/basic-page.component';
 
 export const routes: Routes = [
     {
         path: 'basic',
         title: 'Pipes Básicos',
-        loadComponent: () => import('./pages/basic-page/basic-page.component')
+        component: BasicPageComponent
     },
     {
         path: 'numbers',
