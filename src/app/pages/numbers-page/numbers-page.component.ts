78709216a09bbf96d2d19da3ff83944099ee1bcb
backoffice-tributosimple <gonzaloacosta883@gmail.com>
2025-10-16 17:57:55 -0300

    Feature: DatePipes + Actualización mediante effect.

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,9 +1,9 @@
-import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { CurrencyPipe, DatePipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe, DatePipe],
   templateUrl: './numbers-page.component.html',
 })
 export default class NumbersPageComponent {
@@ -11,4 +11,19 @@ export default class NumbersPageComponent {
   totalSells = signal(2_433_232.5567);
   percent = signal(0.4856);
   
+  customDate = signal(new Date());
+
+  // Actualizar cada 1 segundo la fecha.
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date())
+      console.log("tick");
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+
+  });
+
 }
