1cca8c8c576d364f9f4484c34fd57b6214d891ad
backoffice-tributosimple <gonzaloacosta883@gmail.com>
2025-10-24 12:11:08 -0300

    Feature: i18nSelectPipe. Ej: Si se quiere modificar un texto en base al genero de la persona como invitarlo o invitarla.

--- src/app/pages/uncommont-page/uncommont-page.component.ts
+++ src/app/pages/uncommont-page/uncommont-page.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Fernando',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canada'
+}
+
+const client2 = {
+  name: 'Melisa',
+  gender: 'female',
+  age: 33,
+  address: 'Toronto, Canada'
+}
 
 @Component({
   selector: 'app-uncommont-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommont-page.component.html',
 })
-export default class UncommontPageComponent { }
+export default class UncommontPageComponent {
+
+  // i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  };
+
+  changeClient() {
+
+    if(this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+
+  }
+
+}
