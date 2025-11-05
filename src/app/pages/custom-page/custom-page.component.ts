5805c4f43cadbecd066592fa00f866089e605dd3
backoffice-tributosimple <gonzaloacosta883@gmail.com>
2025-11-05 17:14:16 -0300

    Feature: Pipe Personalizado - CanFlyPipe y HeroColor

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,15 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
 
   nombre = signal('Gonzalo Acosta');
   upperCase = signal(true);
+
+  heroes = signal(heroes);
 
   toggleNombre() {
     this.upperCase.update(value => !value);
