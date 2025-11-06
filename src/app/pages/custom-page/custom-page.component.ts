f25958fa6f39772ab43a0522c7f3b693cf625597
backoffice-tributosimple <gonzaloacosta883@gmail.com>
2025-11-06 14:52:03 -0300

    Feature: Pipe personalizado - HeroSortBy

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,14 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { heroes } from '../../data/heroes.data';
+
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
 import { CanFlyPipe } from '../../pipes/can-fly.pipe';
 import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, HeroCreatorPipe],
+  imports: [
+    ToggleCasePipe, 
+    CanFlyPipe, 
+    HeroColorPipe, 
+    HeroTextColorPipe, 
+    HeroCreatorPipe,
+    HeroSortByPipe
+  ],
   templateUrl: './custom-page.component.html',
 })
 export default class CustomPageComponent {
@@ -17,6 +27,8 @@ export default class CustomPageComponent {
   upperCase = signal(true);
 
   heroes = signal(heroes);
+
+  sortBy = signal< keyof(Hero) | null >(null);
 
   toggleNombre() {
     this.upperCase.update(value => !value);
